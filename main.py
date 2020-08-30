53b6d8783a1c0a3e2a7b5a0f496aa50f0ba65cb4
Leon Kolanowski <lkolanowski3@gmail.com>
2020-08-30 14:17:48 +0200

    temp

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import gpiozero
 import time
+import adafruit_dht
 
+
+#Globale Variablen
 isOpen = False
-
 aufP = "BOARD13"
 aufM = "BOARD11"
 zuP = "BOARD18"
 zuM = "BOARD16"
 delay = 5
 
+#Initialisieren der Relais und Sensoren
 relayAP = gpiozero.OutputDevice(pin=aufP, active_high=True, initial_value=False)
 relayAM = gpiozero.OutputDevice(pin=aufM, active_high=True, initial_value=False)
 relayZP = gpiozero.OutputDevice(pin=zuP, active_high=True, initial_value=False)
 relayZM = gpiozero.OutputDevice(pin=zuM, active_high=True, initial_value=False)
+tempSensor = adafruit_dht.DHT11(board.D18)
 
 
-
+#Alle Relais ausschalten
 def clear():
     relayAM.off()
     relayAP.off()
     relayZP.off()
     relayZM.off()
 
+#Funktion zum Öffnen des Motors
 def openMotor():
     global isOpen
     if(isOpen == False):
@@ -34,6 +39,7 @@ def openMotor():
     else:
         return False
 
+#Funktion zum Schließen des Motors
 def closeMotor():
     global isOpen
     if(isOpen == False):
@@ -46,6 +52,30 @@ def closeMotor():
         isOpen = False
         return True
 
-openMotor()
-time.sleep(2)
-closeMotor()
+#Funktion zum Auslesen der Temperatur
+def readTemp():
+    try:
+        temp = tempSensor.temperature
+        print("Read the temperature at " + temp + "C")
+        return temp
+    except RuntimeError as e:
+        print(e.args[0])
+        return None
+
+
+#Hauptschleife
+while True:
+    time.sleep(120)
+    tempC = readTemp()
+    if tempC is not None:
+        #Temperaturvergleich
+        if(tempC > 30):
+            if isOpen == False:
+                openMotor()
+        elif(tempC < 20):
+            if isOpen == True:
+                closeMotor()
+                
+
+
+
