fc447abc4b81c27c9a9d88915f9d40db541717b6
Leon Kolanowski <lkolanowski3@gmail.com>
2020-08-30 17:58:58 +0200

    changes

--- main.py
+++ main.py
@@ -79,8 +79,6 @@ def readTemp():
 #Hauptschleife
 while True:
     event,values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        break
     tempC = readTemp()
     print(1)
     tempText.update(value=tempC)
@@ -93,6 +91,8 @@ while True:
         elif(tempC < 25):
             if isOpen == True:
                 closeMotor()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
     
 window.close()
 
