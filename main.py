e277cecef0bfd137e4649feb9f03fece5476d4b8
Leon Kolanowski <lkolanowski3@gmail.com>
2020-08-30 18:08:58 +0200

    chanes

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ print("1")
 #Hauptschleife
 while True:
     print("2")
-    event,values = window.read()
     print("3")
     tempC = readTemp()
     print("4")
@@ -92,6 +91,7 @@ while True:
         elif(tempC < 25):
             if isOpen == True:
                 closeMotor()
+    event,values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
     
