11beb7f23c2fe39c9fe2a8985c8081796a73fcd6
Leon Kolanowski <lkolanowski3@gmail.com>
2020-09-16 18:01:15 +0200

    x

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.ui = Ui_main()
         self.ui.setupUi(self)
 
-ApplicationWindow.showFullScreen(ApplicationWindow)
+ApplicationWindow.showFullScreen(Ui_main)
 #Initialisieren der Relais und Sensoren
 relayAP = gpiozero.OutputDevice(pin=aufP, active_high=True, initial_value=False)
 relayAM = gpiozero.OutputDevice(pin=aufM, active_high=True, initial_value=False)
